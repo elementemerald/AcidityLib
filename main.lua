053c68c8f142a8c3c252bb7bb70fdcda6950bdd2
elementemerald <22267679+elementemerald@users.noreply.github.com>
2021-07-22 19:47:52 -0600

    Update some things

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ if CoreGui:FindFirstChild("ACIDITYLIB") then
 end;
 
 local UserInputService = game:GetService("UserInputService");
+local TweenService = game:GetService("TweenService");
 
 local Players = game:GetService("Players");
 local LocalPlayer = Players.LocalPlayer;
@@ -26,12 +27,47 @@ end;
 function library:Window(name)
     local LibUIWindow = Instance.new("Frame");
     LibUIWindow.Name = "ACIDITYLIB_WINDOW_" ..name;
-    LibUIWindow.BorderSizePixel = 0;
-    LibUIWindow.BackgroundColor3 = Color3.fromRGB(60, 60, 60);
-    LibUIWindow.Position = UDim2.fromOffset(100, 20);
-    LibUIWindow.Size = UDim2.fromOffset(207, 33);
+    LibUIWindow.AnchorPoint = Vector2.new(0.5, 0.5);
+    LibUIWindow.BackgroundColor3 = Color3.fromRGB(45, 45, 45);
+    LibUIWindow.Position = UDim2.fromScale(0.5, 0.5);
+    LibUIWindow.Size = UDim2.fromOffset(700, 400);
     LibUIWindow.Active = true;
     LibUIWindow.Parent = LibUI;
+    
+    local LibUIWindowCorner = Instance.new("UICorner");
+    LibUIWindowCorner.CornerRadius = UDim.new(0, 7);
+    LibUIWindowCorner.Parent = LibUIWindow;
+    
+    local LibUIWindowLabel = Instance.new("TextLabel");
+    LibUIWindowLabel.Name = "ACIDITYLIB_WINDOW_" ..name .."_LABEL";
+    LibUIWindowLabel.AnchorPoint = Vector2.new(0.5, 0.5);
+    LibUIWindowLabel.Position = UDim2.fromScale(0.5, 0.5);
+    LibUIWindowLabel.Font = Enum.Font.RobotoCondensed;
+    LibUIWindowLabel.TextSize = 15;
+    LibUIWindowLabel.TextColor3 = Color3.fromRGB(255, 255, 255);
+    LibUIWindowLabel.Text = name;
+    LibUIWindowLabel.Parent = LibUIWindow;
+    
+    local LibUIWindowShowHide = Instance.new("TextButton");
+    LibUIWindowShowHide.Name = "ACIDITYLIB_WINDOW_" ..name .."_SHOWHIDE";
+    LibUIWindowShowHide.TextSize = 12;
+    LibUIWindowShowHide.TextColor3 = Color3.fromRGB(255, 255, 255);
+    LibUIWindowShowHide.Text = "▼";
+    LibUIWindowShowHide.Position = UDim2.fromOffset(10, 10);
+    LibUIWindowShowHide.Parent = LibUIWindow;
+    
+    LibUIWindowShowHide.MouseButton1Click:Connect(function()
+        print("ok");
+        if LibUIWindow.Size.X.Offset == 700 and LibUIWindow.Size.Y.Offset == 400 then
+            TweenService:Create(LibUIWindow, TweenInfo.new(1.0, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {
+                Size = UDim2.fromOffset(0, 0)
+            }):Play();
+        else
+            TweenService:Create(LibUIWindow, TweenInfo.new(1.0, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {
+                Size = UDim2.fromOffset(700, 400)
+            }):Play();
+        end;
+    end);
 
     coroutine.resume(coroutine.create(function()
         local dragging = false;
@@ -69,6 +105,18 @@ function library:Window(name)
 
     local WindowFields = {};
     WindowFields.Instance = LibUIWindow;
+
+    function WindowFields:Open()
+        TweenService:Create(LibUIWindow, TweenInfo.new(), {
+            Size = UDim2.fromOffset(700, 400)
+        }):Play();
+    end;
+
+    function WindowFields:Close()
+        TweenService:Create(LibUIWindow, TweenInfo.new(1.0, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {
+            Size = UDim2.fromOffset(0, 0)
+        }):Play();
+    end;
 
     function WindowFields:Tab(name)
         local LibUIWindowTab = Instance.new("Frame");
